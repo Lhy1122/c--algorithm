af0fec1cb027f2e74ecfef2e314c87768a28f559
Lhy1122 <1319628965@qq.com>
2022-06-21 21:21:52 +0800

    study

---
+++ 02-Sorting-Basic/03-Selection-Sort-Generate-Test-Cases/SortTestHelper.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+namespace SortTestHelper{
+
+    int* generateRandomArray(int n, int rangeL, int rangeR){
+
+        assert(rangeL <= rangeR);
+
+        int *arr = new int[n];
+        srand(time(NULL));
+        for(int i = 0; i < n; i++){
+            arr[i] = rand() % ( rangeR - rangeL + 1) + rangeL;
+        }
+        return arr;
+    }
+}
