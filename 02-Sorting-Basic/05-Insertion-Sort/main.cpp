7f9e1c8f50252c09f014cd3f21f89e3e2f2691d8
Lhy1122 <1319628965@qq.com>
2022-06-21 22:52:39 +0800

    study

---
+++ 02-Sorting-Basic/05-Insertion-Sort/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "SortTestHelper.h"
+#include "SelectionSort.h"
+
+using namespace std;
+
+template<typename T>
+void insertionSort(T arr[], int n){
+
+    for(int i = 1; i < n; i++){
+        //寻找元素arr[i]合适的插入位置
+        for(int j = i; j > 0 && arr[j] < arr[j - 1]; j--){
+            
+            swap(arr[j], arr[j - 1]);
+        }
+    }
+}
+
+int main(){
+
+    int n = 10000;
+    int *arr = SortTestHelper::generateRandomArray(n, 0, n);
+    int *arr2 = SortTestHelper::copyIntArray(arr, n );
+
+    SortTestHelper::testSort("Insertion Sort", insertionSort, arr, n);
+    SortTestHelper::testSort("Selection Sort", selectionSort, arr2, n);
+
+    delete[] arr;
+    delete[] arr2;
+
+    return 0;
+}
