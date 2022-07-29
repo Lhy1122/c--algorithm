cddda5ff83e3ac4750b3ebd8266da6db0a8eff0d
Lhy1122 <1319628965@qq.com>
2022-07-29 21:12:47 +0800

    study

---
+++ 05-Binary-Search-Tree/01-Binary-Search/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+int binarySearch(T arr[], int n, T target){
+
+    int l = 0, r = n - 1;
+    while(l <= r){
+        // int mid = (l + r) / 2; (会造成int型溢出)
+        int mid = l + (r - l) / 2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        
+        if(target < arr[mid]){
+            r = mid - 1;
+        }
+        else{
+            l = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+
+
+
+    return 0;
+}
---
+++ 05-Binary-Search-Tree/02-Binary-Search-Tree-Basics/main.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: Lhy1122 1319628965@qq.com
+ * @Date: 2022-07-29 21:00:54
+ * @LastEditors: Lhy1122 1319628965@qq.com
+ * @LastEditTime: 2022-07-29 21:01:03
+ * @FilePath: \c++-algorithm\05-Binary-Search-Tree\02-Binary-Search-Tree-Basics\main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
