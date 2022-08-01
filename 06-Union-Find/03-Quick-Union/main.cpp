6211f9628f0620a56107be2bf86bec8f9b7ea09e
Lhy1122 <1319628965@qq.com>
2022-08-01 22:14:15 +0800

    study

---
+++ 06-Union-Find/03-Quick-Union/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "UnionFindTestHelper.h"
+
+using namespace std;
+
+int main() {
+    int n = 10000;
+
+    UnionFindTestHelper::testUF1(n);
+
+    UnionFindTestHelper::testUF2(n);
+
+    return 0;
+}
