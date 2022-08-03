23b06184719deff828db16718818a1a06be5e496
Lhy1122 <1319628965@qq.com>
2022-08-03 22:26:03 +0800

    study

--- 07-Graph-Basics/03-Vertex Adjacent Iterator/DenseGraph.h
+++ 07-Graph-Basics/03-Vertex Adjacent Iterator/DenseGraph.h
@@ -1,5 +1,5 @@
-#ifndef INC_02_GRAPH_REPRESENTATION_DENSEGRAPH_H
-#define INC_02_GRAPH_REPRESENTATION_DENSEGRAPH_H
+#ifndef INC_03_VERTEX_ADJACENT_ITERATOR_DENSEGRAPH_H
+#define INC_03_VERTEX_ADJACENT_ITERATOR_DENSEGRAPH_H
 
 #include <iostream>
 #include <vector>
@@ -15,11 +15,11 @@ private:
     vector<vector<bool>> g;
 
 public:
-    
+    // 构造函数
     DenseGraph( int n , bool directed ){
         assert( n >= 0 );
         this->n = n;
-        this->m = 0;
+        this->m = 0;    
         this->directed = directed;
         
         g = vector<vector<bool>>(n, vector<bool>(n, false));
@@ -50,6 +50,42 @@ public:
         assert( w >= 0 && w < n );
         return g[v][w];
     }
+
+    class adjIterator{
+    private:
+        DenseGraph &G;
+        int v;
+        int index;
+
+    public:
+        
+        adjIterator(DenseGraph &graph, int v): G(graph){
+            assert( v >= 0 && v < G.n );
+            this->v = v;
+            this->index = -1;
+        }
+
+        ~adjIterator(){}
+
+        int begin(){
+
+            index = -1;
+            return next();
+        }
+
+        int next(){
+
+            for( index += 1 ; index < G.V() ; index ++ )
+                if( G.g[v][index] )
+                    return index;
+            
+            return -1;
+        }
+
+        bool end(){
+            return index >= G.V();
+        }
+    };
 };
 
-#endif
+#endif 
