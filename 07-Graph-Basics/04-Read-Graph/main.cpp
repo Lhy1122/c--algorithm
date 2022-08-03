6811dce12816ffce195b7bd2f7644ef00ab4b489
Lhy1122 <1319628965@qq.com>
2022-08-03 22:39:33 +0800

    study

--- 07-Graph-Basics/04-Read-Graph/main.cpp
+++ 07-Graph-Basics/04-Read-Graph/main.cpp
@@ -1,51 +1,39 @@
 #include <iostream>
 #include "SparseGraph.h"
 #include "DenseGraph.h"
-#include <ctime>
+#include "ReadGraph.h"
 
 using namespace std;
 
-
 int main() {
 
-    int N = 20;
-    int M = 100;
-
-    srand( time(NULL) );
-
-
-    SparseGraph g1(N , false);
-    for( int i = 0 ; i < M ; i ++ ){
-        int a = rand()%N;
-        int b = rand()%N;
-        g1.addEdge( a , b );
-    }
-
-    for( int v = 0 ; v < N ; v ++ ){
-        cout<<v<<" : ";
-        SparseGraph::adjIterator adj( g1 , v );
-        for( int w = adj.begin() ; !adj.end() ; w = adj.next() )
-            cout<<w<<" ";
-        cout<<endl;
-    }
+    string filename = "testG1.txt";
+    SparseGraph g1( 13 , false );
+    ReadGraph<SparseGraph> readGraph1( g1 , filename );
+    cout<<"test G1 in Sparse Graph:" << endl;
+    g1.show();
 
     cout<<endl;
 
+    DenseGraph g2( 13 , false );
+    ReadGraph<DenseGraph> readGraph2( g2 , filename );
+    cout<<"test G1 in Dense Graph:" << endl;
+    g2.show();
 
-    DenseGraph g2(N , false);
-    for( int i = 0 ; i < M ; i ++ ){
-        int a = rand()%N;
-        int b = rand()%N;
-        g2.addEdge( a , b );
-    }
+    cout<<endl;
 
-    for( int v = 0 ; v < N ; v ++ ){
-        cout<<v<<" : ";
-        DenseGraph::adjIterator adj( g2 , v );
-        for( int w = adj.begin() ; !adj.end() ; w = adj.next() )
-            cout<<w<<" ";
-        cout<<endl;
-    }
+    filename = "testG2.txt";
+    SparseGraph g3( 6 , false );
+    ReadGraph<SparseGraph> readGraph3( g3 , filename );
+    cout<<"test G2 in Sparse Graph:" << endl;
+    g3.show();
+
+    cout<<endl;
+
+    DenseGraph g4( 6 , false );
+    ReadGraph<DenseGraph> readGraph4( g4 , filename );
+    cout<<"test G2 in Dense Graph:" << endl;
+    g4.show();
 
     return 0;
 }
